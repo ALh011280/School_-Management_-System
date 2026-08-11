ec79add79fb584c04072fab8d86921d4ee1f1f6e
Ahmed Raza <abumujtaba1998@gmail.com>
2026-08-11 16:06:44 +0500

    Buttons Added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function test(){
-    alert("This is just testing");
+function first_test(){
+    alert("This is first testing");
+};
+
+function second_test(){
+    alert("This is second testing");
+};
+
+function third_test(){
+    alert("This is third testing");
 };
